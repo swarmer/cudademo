dd14f3098d5245d67ae493f67aff6a54786a7759
Anton Barkovsky <anton@swarmer.me>
2018-10-22 17:13:18 +0300

    Fix SDL drawing demo

--- src/nbody.cpp
+++ src/nbody.cpp
@@ -1,63 +1,106 @@
-#include <iostream>
+#include <cstdint>
+#include <memory>
+
 #include <SDL.h>
 
-int main(int argc, char ** argv)
-{
-    bool leftMouseButtonDown = false;
-    bool quit = false;
-    SDL_Event event;
+using std::unique_ptr;
 
-    SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window * window = SDL_CreateWindow("SDL2 Pixel Drawing",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+class NBodyRenderer {
+    const int m_width, m_height;
 
-    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
-    SDL_Texture * texture = SDL_CreateTexture(renderer,
-        SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 640, 480);
-    Uint32 * pixels = new Uint32[640 * 480];
+    uint32_t *pixels;
 
-    memset(pixels, 255, 640 * 480 * sizeof(Uint32));
-
-    while (!quit)
+public:
+    NBodyRenderer(int width = 640, int height = 480)
+        : m_width{width}, m_height{height}
     {
-        SDL_UpdateTexture(texture, NULL, pixels, 640 * sizeof(Uint32));
-
-        SDL_WaitEvent(&event);
-
-        switch (event.type)
-        {
-            case SDL_QUIT:
-                quit = true;
-                break;
-            case SDL_MOUSEBUTTONUP:
-                if (event.button.button == SDL_BUTTON_LEFT)
-                    leftMouseButtonDown = false;
-                break;
-            case SDL_MOUSEBUTTONDOWN:
-                if (event.button.button == SDL_BUTTON_LEFT)
-                    leftMouseButtonDown = true;
-            case SDL_MOUSEMOTION:
-                if (leftMouseButtonDown)
-                {
-                    int mouseX = event.motion.x;
-                    int mouseY = event.motion.y;
-                    pixels[mouseY * 640 + mouseX] = 0;
-                }
-                break;
-        }
-
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
-        SDL_RenderPresent(renderer);
+        pixels = new uint32_t[width * height];
+        memset(pixels, 0, width * height * sizeof(uint32_t));
     }
 
-    delete[] pixels;
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
+    NBodyRenderer(const NBodyRenderer&) = delete;
+    NBodyRenderer& operator=(const NBodyRenderer&) = delete;
+    NBodyRenderer(NBodyRenderer&&) = default;
+    NBodyRenderer& operator=(NBodyRenderer&&) = default;
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    void update() {
+        // TODO
+    }
 
+    int width() { return m_width; }
+    int height() { return m_height; }
+    const uint32_t* get_buffer() { return pixels; }
+
+    ~NBodyRenderer() {
+        delete[] pixels;
+    }
+};
+
+
+class NBodySDLWindow {
+    const unique_ptr<NBodyRenderer> nbody_renderer;
+
+    SDL_Window *sdl_window;
+    SDL_Renderer *sdl_renderer;
+    SDL_Texture *sdl_texture;
+
+public:
+    explicit NBodySDLWindow(unique_ptr<NBodyRenderer> nbody_renderer_)
+        : nbody_renderer{std::move(nbody_renderer_)}
+    {
+        SDL_Init(SDL_INIT_VIDEO);
+        sdl_window = SDL_CreateWindow(
+            "NBody Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            nbody_renderer->width(), nbody_renderer->height(), 0
+        );
+        sdl_renderer = SDL_CreateRenderer(sdl_window, -1, 0);
+        sdl_texture = SDL_CreateTexture(
+            sdl_renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC,
+            nbody_renderer->width(), nbody_renderer->height()
+        );
+    }
+
+    NBodySDLWindow(const NBodySDLWindow&) = delete;
+    NBodySDLWindow& operator=(const NBodySDLWindow&) = delete;
+    NBodySDLWindow(NBodySDLWindow&&) = default;
+    NBodySDLWindow& operator=(NBodySDLWindow&&) = default;
+
+    void run_event_loop() {
+        while (true)
+        {
+            // handle exit events
+            SDL_Event event;
+            SDL_WaitEvent(&event);
+            if (event.type == SDL_QUIT) {
+                break;
+            }
+
+            // run and render a simulation step
+            nbody_renderer->update();
+
+            // draw a newly rendered frame
+            SDL_UpdateTexture(
+                sdl_texture, NULL, nbody_renderer->get_buffer(),
+                nbody_renderer->width() * sizeof(uint32_t)
+            );
+            SDL_RenderClear(sdl_renderer);
+            SDL_RenderCopy(sdl_renderer, sdl_texture, NULL, NULL);
+            SDL_RenderPresent(sdl_renderer);
+        }
+    }
+
+    ~NBodySDLWindow() {
+        SDL_DestroyTexture(sdl_texture);
+        SDL_DestroyRenderer(sdl_renderer);
+        SDL_DestroyWindow(sdl_window);
+        SDL_Quit();
+    }
+};
+
+
+int main(int argc, char *argv[]) {
+    NBodySDLWindow nbody_window{std::make_unique<NBodyRenderer>()};
+    nbody_window.run_event_loop();
     return 0;
 }
